9318429af2e19a8c01a7ef56fcde7fea2e555a86
Marius Linders <marius.linders@orange-juice.nl>
2017-11-24 09:19:41 +0100

    rewrite npm module

--- README.md
+++ README.md
@@ -11,32 +11,32 @@ const ea: EventAggregator = new EventAggregator();
 
 ### emit an event
 ```typescript
-window.addEventListener("resize", (event) => ea.emit("resize", event));
+window.addEventListener("resize", (event: any): void => ea.emit("resize", event));
 ```
 
 ### subscribe to an event
 ```typescript
-ea.on("resize", (data) => {
+ea.on("resize", (data: any, event: string): void => {
   // do something on resize
 });
 ```
 
 ### subscribe to only the first event
 ```typescript
-ea.once("resize", (data) => {
+ea.once("resize", (data: any, event: string): void => {
   // do something on the first resize event
 });
 ```
 
 ### unsubscribe a subscription
 ```typescript
-const subscription: ISubscription = ea.on("resize", (data) => {...});
+const subscription: IUnsubscribe = ea.on("resize", (data: any, event: string): void => {...});
 subscription.off();
 ```
 
 ### subscribe to all events of the EventAggregator
 ```typescript
-ea.on("*", (data, originalEvent) => {
+ea.on("*", (data: any, event: string): void => {
   // handle all events
   // originalEvent holds the event name, for example: "resize"
 });
@@ -46,26 +46,26 @@ ea.on("*", (data, originalEvent) => {
 ```typescript
 const subs: ISubscription[] = [];
 
-subs.push(ea.on("resize", (data) => {
+subs.push(ea.on("resize", (data: any, event: string): void => {
   // do something on resize
 }));
 
-function unsubscribeAll(() {
-  subs.forEach((sub: ISubscription) => sub.off());
+function unsubscribeAll(): void {
+  subs.forEach((sub: ISubscription): void => sub.off());
   subs = [];
 }
 ```
 
 ### throttle an event
 ```typescript
-ea.on("very-frequent-event", throttle((data) => {
+ea.on("very-frequent-event", throttle((data: any, event: string): void => {
   // handle the events with a maximum of one per 500 milliseconds (default threshhold is 100 milliseconds)
 }, 500));
 ```
 
 ### debounce an event
 ```typescript
-ea.on("very-frequent-event", debounce((data) => {
+ea.on("very-frequent-event", debounce((data: any, event: string): void => {
   // handle the last event and only if the event has been silence for 500 milliseconds (default threshhold is 100 milliseconds)
 }, 500));
 ```
---
+++ index.js
@@ -0,0 +1,117 @@
+"use strict";
+exports.__esModule = true;
+var EventAggregator = /** @class */ (function () {
+    function EventAggregator() {
+        this.subs = {};
+        this._id = 0;
+    }
+    EventAggregator.getInstance = function (type) {
+        if (type === void 0) { type = "main"; }
+        if (EventAggregator.instances[type] === undefined) {
+            EventAggregator.instances[type] = new EventAggregator();
+        }
+        return EventAggregator.instances[type];
+    };
+    EventAggregator.prototype.on = function (event, fn, once) {
+        var _this = this;
+        if (once === void 0) { once = false; }
+        if (Array.isArray(event)) {
+            var subs_1 = event.map(function (e) { return _this.on(e, fn, once); });
+            return {
+                off: function () { return subs_1.forEach(function (sub) { return sub.off(); }); }
+            };
+        }
+        else {
+            return this.addSub(event, fn, once);
+        }
+    };
+    EventAggregator.prototype.once = function (event, fn) {
+        return this.on(event, fn, true);
+    };
+    EventAggregator.prototype.emit = function (event, data) {
+        var _this = this;
+        if (Array.isArray(event)) {
+            event.forEach(function (e) { return _this.emit(e, data); });
+        }
+        else {
+            this.emitSubs(event, data, event);
+            this.emitSubs("*", data, event);
+        }
+        return this;
+    };
+    EventAggregator.prototype.off = function (event, id) {
+        this.subs[event].splice(this.subs[event].findIndex(function (sub) { return sub._id === id; }), 1);
+        return this;
+    };
+    EventAggregator.prototype.addSub = function (event, fn, once) {
+        var _this = this;
+        if (once === void 0) { once = false; }
+        if (this.subs[event] === undefined) {
+            this.subs[event] = [];
+        }
+        var id = this.nextId;
+        this.subs[event].push({ _id: id, _fn: fn, once: once });
+        return { off: function () { return _this.off(event, id); } };
+    };
+    EventAggregator.prototype.emitSubs = function (event, data, originalEvent) {
+        if (this.subs[event] !== undefined) {
+            for (var i = 0; i < this.subs[event].length; i++) {
+                var sub = this.subs[event][i];
+                if (typeof sub._fn === "function") {
+                    sub._fn(data, originalEvent);
+                    if (sub.once === true) {
+                        this.off(event, sub._id);
+                        i--;
+                    }
+                }
+                else {
+                    this.off(event, sub._id);
+                    i--;
+                }
+            }
+        }
+    };
+    Object.defineProperty(EventAggregator.prototype, "nextId", {
+        get: function () {
+            return this._id++;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    EventAggregator.instances = {};
+    return EventAggregator;
+}());
+exports["default"] = EventAggregator;
+exports.debounce = function (fn, threshhold, scope) {
+    if (threshhold === void 0) { threshhold = 100; }
+    if (scope === void 0) { scope = null; }
+    var deferTimer;
+    return function () {
+        var args = arguments;
+        clearTimeout(deferTimer);
+        deferTimer = setTimeout(function () {
+            fn.apply(scope, args);
+        }, threshhold);
+    };
+};
+exports.throttle = function (fn, threshhold, scope) {
+    if (threshhold === void 0) { threshhold = 100; }
+    if (scope === void 0) { scope = null; }
+    var last;
+    var deferTimer;
+    return function () {
+        var now = Date.now();
+        var args = arguments;
+        if (last && now < last + threshhold) {
+            clearTimeout(deferTimer);
+            deferTimer = setTimeout(function () {
+                last = now;
+                fn.apply(scope, args);
+            }, threshhold);
+        }
+        else {
+            last = now;
+            fn.apply(scope, args);
+        }
+    };
+};
